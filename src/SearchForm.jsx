0f72f24d8846ddf1213acaef37553ef1f54aed46
Daniel-K <daniel.rt5d@gmail.com>
2026-08-27 15:38:31 -0500

    feat: implementar formulario de búsqueda con manejo de envío

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const SearchForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const searchValues = e.target.elements.search.value;
+        if (!searchValues) return;
+    }
   return (
-    <div>SearchForm</div>
+    <section>
+        <h1 className="title"> almacenamiento de imágenes </h1>
+        <form className="search-form" onSubmit={handleSubmit}>
+            <input type="text" name="search" className="form-input search-input" placeholder="Animal" />
+            <button type="submit" className="btn">buscar</button>
+        </form>
+    </section>
   )
 }
 
